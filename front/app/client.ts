7c83f651b987a842836c26e57b2d4fca67593b3f
tota-kn <nonbiriactive@gmail.com>
2025-07-05 16:35:00 +0900

    update

--- front/app/client.ts
+++ front/app/client.ts
@@ -2,6 +2,6 @@ import type { ClientType } from "../../shared/client";
 import { hc } from "hono/client";
 
 export const createClient = (env: Env) =>
-  hc<ClientType>("http://localhost:8787", {
+  hc<ClientType>("http://example/", {
     fetch: env.BACKEND_API.fetch.bind(env.BACKEND_API),
   });
