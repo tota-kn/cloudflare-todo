0c8250548e5fbc8805d7b0b483570527517c43bc
tota-kn <nonbiriactive@gmail.com>
2025-07-05 22:28:00 +0900

    update

--- back/src/function/r2/post.ts
+++ back/src/function/r2/post.ts
@@ -1,14 +1,15 @@
 import { Hono } from 'hono'
 
 export const r2Post = new Hono<{ Bindings: CloudflareEnv }>()
-  .post('/:key', async (c) => {
+  .post('', async (c) => {
     const key = c.req.param('key')
     const body = await c.req.raw.arrayBuffer()
-    
+
     try {
       await c.env.STORAGE.put(key, body)
       return c.json({ message: `File ${key} uploaded successfully` })
-    } catch (error) {
+    }
+    catch (error) {
       return c.json({ error: 'Failed to upload file' }, 500)
     }
   })
