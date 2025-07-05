e2b3da6cf08368f00168be96a776ab45ab386b5a
tota-kn <nonbiriactive@gmail.com>
2025-07-05 18:43:34 +0900

    update

--- back/src/function/get.ts
+++ back/src/function/get.ts
@@ -1,6 +1,6 @@
+import { zValidator } from '@hono/zod-validator'
 import { Hono } from 'hono'
 import { z } from 'zod'
-import { zValidator } from '@hono/zod-validator'
 
 export const indexGet = new Hono<{ Bindings: CloudflareEnv }>().get(
   '',
--- back/src/index.ts
+++ back/src/index.ts
@@ -1,13 +1,13 @@
 import { Hono } from 'hono'
 import { cors } from 'hono/cors'
-import { v1Get } from './function/v1/get'
 import { indexGet } from './function/get'
+import { v1Get } from './function/v1/get'
 
 const app = new Hono<{ Bindings: CloudflareEnv }>()
   .use(
     '*',
     cors({
-      origin: (origin, c) => c.env.CORS_ORIGIN,
+      origin: (_origin, c) => c.env.CORS_ORIGIN,
       allowHeaders: ['Content-Type', 'Authorization'],
       allowMethods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     }),
