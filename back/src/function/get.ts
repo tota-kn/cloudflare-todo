0053517d21a8f7928a20c192244346b293467054
tota-kn <nonbiriactive@gmail.com>
2025-07-05 15:52:49 +0900

    update

--- back/src/function/get.ts
+++ back/src/function/get.ts
@@ -2,7 +2,7 @@ import { Hono } from 'hono'
 import { z } from 'zod'
 import { zValidator } from '@hono/zod-validator'
 
-export const indexGet = new Hono().get(
+export const indexGet = new Hono<{ Bindings: CloudflareEnv }>().get(
   '',
   zValidator(
     'query',
@@ -12,8 +12,9 @@ export const indexGet = new Hono().get(
   ),
   (c) => {
     const query = c.req.valid('query')
+    const stage = c.env.STAGE
     return c.json({
-      message: `${query.text}: API is OK!`,
+      message: `${stage} is OK; text=${query.text}`,
     })
   },
 )
