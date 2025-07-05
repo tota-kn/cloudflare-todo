1322109386c1cda221979e797a7a0016714e88e6
tota-kn <nonbiriactive@gmail.com>
2025-07-05 18:34:55 +0900

    update

--- front/app/routes.ts
+++ front/app/routes.ts
@@ -2,5 +2,5 @@ import { type RouteConfig, index, route } from "@react-router/dev/routes";
 
 export default [
   index("routes/home.tsx"),
-  route("backend-test", "routes/backend-test.tsx"),
+  route("test", "routes/test.tsx"),
 ] satisfies RouteConfig;
