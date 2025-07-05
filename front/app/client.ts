82726c835279884504724a0ce3699f60f9e21b3d
tota-kn <nonbiriactive@gmail.com>
2025-07-05 17:16:52 +0900

    qupdate

--- front/app/client.ts
+++ front/app/client.ts
@@ -1,7 +1,11 @@
 import type { ClientType } from "../../shared/client";
 import { hc } from "hono/client";
 
-export const createClient = (env: Env) =>
-  hc<ClientType>("http://example/", {
+export const createClient = (env: Env) => {
+  if (env.IS_LOCAL === "true") {
+    return hc<ClientType>("http://localhost:8787/");
+  }
+  return hc<ClientType>("http://example/", {
     fetch: env.BACKEND_API.fetch.bind(env.BACKEND_API),
   });
+};
