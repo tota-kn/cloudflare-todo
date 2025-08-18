152166bb4265e90ac8e80f089ea695e6876ebb7f
tota-kn <nonbiriactive@gmail.com>
2025-08-18 23:05:21 +0900

    update

--- front/app/routes.ts
+++ front/app/routes.ts
@@ -1,8 +1,8 @@
 import { type RouteConfig, route } from "@react-router/dev/routes";
 
 export default [  
-  route("/","routes/home.tsx"),
-  route("/todos", "routes/todos.tsx"),
+  route("/test","routes/home.tsx"),
+  route("/", "routes/todos.tsx"),
 
   // Chrome DevToolsが自動的にリクエストしてエラーログが出てくるため抑制
   route("/.well-known/appspecific/com.chrome.devtools.json", "routes/devtools.tsx"),
