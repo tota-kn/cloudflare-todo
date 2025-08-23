73122692fde805492dc2a2081d7b0bc227e2d533
tota-kn <nonbiriactive@gmail.com>
2025-08-23 19:01:11 +0900

    update

---
+++ front/eslint.config.mjs
@@ -0,0 +1,29 @@
+import { includeIgnoreFile } from '@eslint/compat'
+import eslint from '@eslint/js'
+import stylistic from '@stylistic/eslint-plugin'
+import reactPlugin from 'eslint-plugin-react'
+import reactHooksPlugin from 'eslint-plugin-react-hooks'
+import path from 'node:path'
+import { fileURLToPath } from 'node:url'
+import tseslint from 'typescript-eslint'
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+const gitignorePath = path.resolve(__dirname, '.gitignore')
+
+export default tseslint.config(
+  eslint.configs.recommended,
+  tseslint.configs.recommended,
+  stylistic.configs.recommended,
+  reactPlugin.configs.flat.recommended,
+  reactPlugin.configs.flat['jsx-runtime'],
+  reactHooksPlugin.configs['recommended-latest'],
+  includeIgnoreFile(gitignorePath),
+  {
+    settings: {
+      react: {
+        version: 'detect',
+      },
+    },
+  },
+)
