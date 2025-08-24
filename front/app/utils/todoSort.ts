649faa62cda7e8bdc81fe189b746296f77ab4ee7
tota-kn <nonbiriactive@gmail.com>
2025-08-24 17:28:05 +0900

    update

--- front/app/utils/todoSort.ts
+++ front/app/utils/todoSort.ts
@@ -8,7 +8,7 @@ export interface TodoWithTimestamp {
 export const sortTodosByUpdatedAt = <T extends TodoWithTimestamp>(
   todos: T[]
 ): T[] => {
-  return todos.sort(
+  return [...todos].sort(
     (a, b) =>
       new Date(b.updated_at).getTime() - new Date(a.updated_at).getTime()
   )
