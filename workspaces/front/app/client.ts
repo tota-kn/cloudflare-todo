51be47e698351b60211d9b04e3f0749fc927290f
tota-kn <nonbiriactive@gmail.com>
2025-09-07 03:50:23 -0700

    update

--- workspaces/front/app/client.ts
+++ workspaces/front/app/client.ts
@@ -16,7 +16,7 @@ export const createBrowserClient = () => {
     typeof window === "undefined" ||
     window.location.host.includes("localhost")
   ) {
-    return hc<ClientType>("http://localhost:8787")
+    return hc<ClientType>("http://127.0.0.1:8787")
   }
 
   return hc<ClientType>(`https://api.${window.location.hostname}`)
