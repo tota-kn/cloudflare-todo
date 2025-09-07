0ec7e196706dab19f5d8fe23bb9554d23ff5865b
tota-kn <nonbiriactive@gmail.com>
2025-09-07 05:40:56 -0700

    update

--- workspaces/back/src/presentation/api/v1/assets/_filename/get.ts
+++ workspaces/back/src/presentation/api/v1/assets/_filename/get.ts
@@ -16,16 +16,30 @@ export function v1AssetsFilenameGet(dependencies: Dependencies) {
 
       try {
         const assetStorage = dependencies.getBucketRepository()
-
         const asset = await assetStorage.get(filename)
-
         if (!asset) {
           return c.json({ error: "File not found" }, 404)
         }
-        const body = await asset.arrayBuffer()
-        return c.body(body, 200, {
+
+        const etag = `"${asset.etag}"`
+        const cacheControl = "public, max-age=86400, immutable" // 1日間キャッシュ
+        if (c.req.header("If-None-Match") === etag) {
+          return new Response(null, {
+            status: 304,
+            headers: {
+              ETag: etag,
+              "Cache-Control": cacheControl,
+            },
+          })
+        }
+
+        const response = c.body(await asset.arrayBuffer(), 200, {
           "Content-Type": asset.httpMetadata?.contentType ?? "image/jpeg",
+          "Cache-Control": cacheControl,
+          ETag: etag,
         })
+
+        return response
       } catch (error) {
         console.error("Error fetching asset:", error)
         return c.json({ error: "Internal server error" }, 500)
